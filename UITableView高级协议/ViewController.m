91a0d3adb957c7d6b845e34115106449302a58d2
ZHDKk <1083861095@qq.com>
2017-11-09 11:04:17 +0800

    第一次提交

--- UITableView高级协议/ViewController.m
+++ UITableView高级协议/ViewController.m
@@ -17,7 +17,82 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    //自动调整子视图的大小
+    _tableView.autoresizingMask = UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth;
+    
+    //设置代理
+    _tableView.delegate = self;
+    _tableView.dataSource  = self;
+    
+    //数据视图的头部和尾部视图设定
+    _tableView.tableHeaderView = nil;
+    _tableView.tableFooterView = nil;
+    
+    [self.view addSubview:_tableView];
+    
+    _arrayData = [[NSMutableArray alloc]init];
+    for (int i=0; i<20; i++) {
+        NSString *str= [NSString stringWithFormat:@"A %d",i];
+        
+        [_arrayData addObject:str];
+    }
+    
+    //当数据视图的数据源发生变化时更新视图数据,重新加载数据
+    [_tableView reloadData];
+    
+    [self createBtn];
+    
 }
+
+-(void) createBtn
+{
+    _isEdit = NO;
+    
+    _btnEdit = [[UIBarButtonItem alloc] initWithTitle:@"编辑" style:UIBarButtonItemStylePlain target:self action:@selector(pressEdit)];
+    _btnDelete = [[UIBarButtonItem alloc] initWithTitle:@"删除" style:UIBarButtonItemStylePlain target:self action:@selector(pressDelete)];
+    _btnFinish = [[UIBarButtonItem alloc] initWithTitle:@"完成" style:UIBarButtonItemStylePlain target:self action:@selector(pressFinish)];
+    
+    self.navigationItem.rightBarButtonItem = _btnEdit;
+    
+}
+
+-(void) pressEdit{
+    _isEdit = YES;
+    self.navigationItem.rightBarButtonItem = _btnFinish;
+    [_tableView setEditing:YES];
+    self.navigationItem.leftBarButtonItem = _btnDelete;
+}
+
+-(void) pressFinish
+{
+    _isEdit = NO;
+    self.navigationItem.rightBarButtonItem = _btnEdit;
+    [_tableView setEditing:NO];
+    self.navigationItem.leftBarButtonItem = nil;
+}
+-(void) pressDelete{
+    
+}
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _arrayData.count;
+}
+-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *str= @"ID";
+    //获取可复用的单元格
+    UITableViewCell *cell = [_tableView dequeueReusableCellWithIdentifier:str];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:str];
+    }
+    //单元格赋值
+    cell.textLabel.text = [_arrayData objectAtIndex:indexPath.row];
+    return cell;
+}
+
 
 
 - (void)didReceiveMemoryWarning {
